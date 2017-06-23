d6cddee39c04d93a4dbcf7f1820003b412c0646f
dahye Jeong <wjdekgp1750@naver.com>
2017-06-23 16:44:09 +0900

    passport signup setting

--- ch09/app.js
+++ ch09/app.js
@@ -102,6 +102,39 @@ passport.use('local-login',new LocalStratege({
   	});
 }));
 
+//패스포트 회원가입 설정
+passport.use('local-signup',new LocalStratege({
+	usernameField : 'email',
+	passwordField : 'password',
+	passReqToCallback : true
+},function(req,email,password,done){
+	//요청 파라미터 중 name파라미터 확인
+	var paramName = req.body.name || req.query.name;
+	console.log('passport의 local-signup호출됨 : '+email+password+paramName);
+
+	//User.findOne이 blocking되도록 async 방식으로 변경할 수도 있다.
+	process.nextTick(function(){
+		var database = app.get('database');
+		database.UserModel.findOne({'email':email},function(err,user){
+			//오류 발생하면
+			if(err){ return done(err);}
+
+			//기존에 이메일이 있다면
+			if(user){
+				console.log('기존에 계정이 있음');
+				return done(null,false,req.flash('signupMessage','계정이 이미 있습니다.'));
+			}else{
+				//모델 인스턴스 객체 만들어 저장
+				var user = new database.UserModel({'email':email,'password':password,'name': paramName});
+				user.save(function(err){
+					if(err){throw err;}
+					console.log('사용자 데이터 추가함.');
+					return done(null,user);
+				});
+			}
+		});
+	});
+}));
 
 //===== 404 에러 페이지 처리 =====//
 var errorHandler = expressErrorHandler({
