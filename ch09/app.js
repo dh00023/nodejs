0a28d3b69340a755b33461356a049d8fc67b2dbf
dahye Jeong <wjdekgp1750@naver.com>
2017-06-23 16:32:32 +0900

    passport login setting

--- ch09/app.js
+++ ch09/app.js
@@ -1,13 +1,3 @@
-/**
- * ejs 뷰 템플릿 적용하기
- * 
- * 뷰 템플릿을 만들고 응답 웹문서를 템플릿으로부터 생성
- *
- * @date 2016-11-10
- * @author Mike
- */
- 
-
 // Express 기본 모듈 불러오기
 var express = require('express')
   , http = require('http')
@@ -25,6 +15,9 @@ var expressErrorHandler = require('express-error-handler');
 // Session 미들웨어 불러오기
 var expressSession = require('express-session');
   
+//passport사용
+var passport = require('passport');
+var flash = require('connect-flash');
 
 // 모듈로 분리한 설정 파일 불러오기
 var config = require('./config');
@@ -34,9 +27,6 @@ var database = require('./database/database');
 
 // 모듈로 분리한 라우팅 파일 불러오기
 var route_loader = require('./routes/route_loader');
-
- 
-
 
 // 익스프레스 객체 생성
 var app = express();
@@ -72,11 +62,45 @@ app.use(expressSession({
 	saveUninitialized:true
 }));
 
- 
+// passport사용설정
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+
 //라우팅 정보를 읽어들여 라우팅 설정
 route_loader.init(app, express.Router());
 
+var LocalStratege = require('passport-local').Strategy;
 
+//패스포트 로그인 설정
+passport.use('local-login',new LocalStratege({
+	usernameField : 'email',
+	passwordField: 'password',
+	passReqToCallback: true
+  },function(req,email,password,done){
+  	console.log('passport의 local-login호출됨'+email+password);
+
+  	var database = app.get('database');
+  	database.UserModel.findOne({'email':email},function(err,user){
+  		if(err){return done(err);}
+
+  		//등록된 사용자가 없는 경우
+  		if(!user){
+  			console.log('계정이 일치하지 않음');
+  			return done(null,false,req.falsh('loginMessage','등록된 계정이 없습니다.'));
+  		}
+  		//비밀번호를 비교하여 맞지 않는 경우
+  		var authenticated = user.authenticate(password,user._doc.salt,user._doc.hashed_password);
+  		if(!authenticated){
+  			console.log('비밀번호 일치하지 않음');
+  			return done(null,false,req.flash('loginMessage','비밀번호가 일치하지 않습니다.'));
+  		}
+
+  		//정상
+  		console.log('계정과 비밀번호가 일치함');
+  		return done(null,user);
+  	});
+}));
 
 
 //===== 404 에러 페이지 처리 =====//
